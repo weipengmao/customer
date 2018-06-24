2a37110c1b97eb7bd0cdd37bb8a34177e37c2634
weipengmao <412948637@qq.com>
2018-06-24 23:10:15 +0800

    修复对话页面部分bug

--- src/components/account.vue
+++ src/components/account.vue
@@ -15,49 +15,86 @@
           </div>
         </div>
 
-        <div class="contentRight" style="width:100%" v-if="firstQuest" v-for="key in firstQuestTxt">
-          <div class="rightContent" style="float:right">
-            <p align="left">{{key}}
-            </p>
+        <div class="cpBefore" v-for="(key,item) in beforeTxt">
+          <div class="contentRight" style="width:100%" v-if="beforeQuest" >
+            <div class="rightContent" style="float:right">
+              <p align="left">{{key}}
+              </p>
+            </div>
           </div>
-        </div>
 
-        <div class="content" v-if="answerTxt" >
-          <img src="../common/image/robot_profile.png" class="contentImg" >
-          <div class="contentBox" style="width:80%;margin-right:0.22rem;" >
-            <p align="left">猪肉作为餐桌上重要的动物性食品之一，因为纤维较为细软，结缔组织较少。
-            </p>
-            <hr style="width:93%;border:0.5px solid rgba(131,205,93,1);margin:0 auto;">
-            <img :src="heart" alt="" style="float:left" @click="changeStyle">
-            <p style="float:right;font-size:0.1rem;font-family:SourceHanSansCN-Normal;color:rgba(77,77,77,1);
-            line-height:0.5rem;" @click="moreAnswer">更多</p>
-          </div>
-        </div>
-
-        <div class="content" style="width:100%" v-if="flagTxt">
-          <img src="../common/image/robot_profile.png" class="contentImg" style="margin-left:0;">
-          <div class="contentBox" style="width:70%;margin-right:0.9rem;">
-            <div v-for="(item,key) in answer">
-              <p align="left">{{item}}
+          <div class="content" v-if="beforeflag" >
+            <img src="../common/image/robot_profile.png" class="contentImg" >
+            <div class="contentBox" style="width:80%;margin-right:0.22rem;" >
+              <p align="left">猪肉作为餐桌上重要的动物性食品之一，因为纤维较为细软，结缔组织较少。
               </p>
               <hr style="width:93%;border:0.5px solid rgba(131,205,93,1);margin:0 auto;">
+              <img :src="heart" alt="" style="float:left" >
+              <p style="float:right;font-size:0.1rem;font-family:SourceHanSansCN-Normal;color:rgba(77,77,77,1);
+            line-height:0.5rem;" >更多</p>
             </div>
-            <img src="../common/image/service_ears.png" alt="" style="float:left;margin-top:0.05rem;" @click="serviceDetail"><span
-          style="font-size:0.1rem;font-family:SourceHanSansCN-Normal;color:rgba(77,77,77,1);
+          </div>
+
+          <div class="content" style="width:100%" v-show="beforeTxt" ref="beforeItem">
+            <img src="../common/image/robot_profile.png" class="contentImg" style="margin-left:0;">
+            <div class="contentBox" style="width:70%;margin-right:0.9rem;">
+              <div v-for="(item,key) in answer">
+                <p align="left">{{item}}
+                </p>
+                <hr style="width:93%;border:0.5px solid rgba(131,205,93,1);margin:0 auto;">
+              </div>
+              <img src="../common/image/service_ears.png" alt="" style="float:left;margin-top:0.05rem;" @click="serviceDetail"><span
+              style="font-size:0.1rem;font-family:SourceHanSansCN-Normal;color:rgba(77,77,77,1);
             float:left;line-height:1.10rem" @click="serviceDetail">转人工客服</span>
-            <p style="float:right;font-size:0.1rem;font-family:SourceHanSansCN-Normal;color:rgba(77,77,77,1);
+              <p style="float:right;font-size:0.1rem;font-family:SourceHanSansCN-Normal;color:rgba(77,77,77,1);
             line-height:0.5rem;" @click="addText">更多</p>
+            </div>
+            <div class="service">
+              <hr style="width:28%;position:absolute;left:0.5rem;top:0.12rem;opacity:0.2">
+              <hr style="width:28%;position:absolute;right:0.5rem;top:0.12rem;opacity:0.2">
+              <p class="serviceText">历史消息</p>
+            </div>
           </div>
         </div>
 
-        <div class="contentRight" style="width:100%" v-if="quest" v-for="key in questTxt">
-          <div class="rightContent" style="float:right">
-            <p align="left">{{key}}
-            </p>
+        <div class="cpOne" v-for="(key,item) in firstQuestTxt">
+          <div class="contentRight" style="width:100%" v-if="firstQuest" >
+            <div class="rightContent" style="float:right">
+              <p align="left">{{key}}
+              </p>
+            </div>
+          </div>
+
+          <div class="content" v-if="answerTxt" >
+            <img src="../common/image/robot_profile.png" class="contentImg" >
+            <div class="contentBox" style="width:80%;margin-right:0.22rem;" >
+              <p align="left">猪肉作为餐桌上重要的动物性食品之一，因为纤维较为细软，结缔组织较少。
+              </p>
+              <hr style="width:93%;border:0.5px solid rgba(131,205,93,1);margin:0 auto;">
+              <img :src="heart" alt="" style="float:left" @click="changeStyle(item)" ref="changeSty">
+              <p style="float:right;font-size:0.1rem;font-family:SourceHanSansCN-Normal;color:rgba(77,77,77,1);
+            line-height:0.5rem;" @click="moreAnswer(item)" >更多</p>
+            </div>
+          </div>
+
+          <div class="content" style="width:100%" v-show="flagTxt" ref="item">
+            <img src="../common/image/robot_profile.png" class="contentImg" style="margin-left:0;">
+            <div class="contentBox" style="width:70%;margin-right:0.9rem;">
+              <div v-for="(item,key) in answer">
+                <p align="left">{{item}}
+                </p>
+                <hr style="width:93%;border:0.5px solid rgba(131,205,93,1);margin:0 auto;">
+              </div>
+              <img src="../common/image/service_ears.png" alt="" style="float:left;margin-top:0.05rem;" @click="serviceDetail"><span
+              style="font-size:0.1rem;font-family:SourceHanSansCN-Normal;color:rgba(77,77,77,1);
+            float:left;line-height:1.10rem" @click="serviceDetail">转人工客服</span>
+              <p style="float:right;font-size:0.1rem;font-family:SourceHanSansCN-Normal;color:rgba(77,77,77,1);
+            line-height:0.5rem;" @click="addText">更多</p>
+            </div>
           </div>
         </div>
 
-        <div class="contentService" v-if="service">
+        <div class="contentService" v-if="service" v-for="(key,item) in firstQuestTxt">
           <div class="service">
             <hr style="width:28%;position:absolute;left:0.5rem;top:0.12rem;opacity:0.2">
             <hr style="width:28%;position:absolute;right:0.5rem;top:0.12rem;opacity:0.2">
@@ -69,11 +106,50 @@
               <p align="left">猪肉作为餐桌上重要的动物性食品之一，因为纤维较为细软，结缔组织较少。
               </p>
               <hr style="width:93%;border:0.5px solid rgba(131,205,93,1);margin:0 auto;">
+              <img :src="heartBeat" alt="" style="float:left" @click="changeBeat(item-1)" ref="changeBeat">
+            </div>
+          </div>
+        </div>
+
+        <div class="cpTwo" v-for="(key,item) in questTxt">
+          <div class="contentRight" style="width:100%" v-if="quest" >
+            <div class="rightContent" style="float:right">
+              <p align="left">{{key}}
+              </p>
+            </div>
+          </div>
+
+          <div class="content" v-if="answerTxt" >
+            <img src="../common/image/robot_profile.png" class="contentImg" >
+            <div class="contentBox" style="width:80%;margin-right:0.22rem;" >
+              <p align="left">猪肉作为餐桌上重要的动物性食品之一，因为纤维较为细软，结缔组织较少。
+              </p>
+              <hr style="width:93%;border:0.5px solid rgba(131,205,93,1);margin:0 auto;">
               <img :src="heart" alt="" style="float:left" @click="changeStyle">
+              <p style="float:right;font-size:0.1rem;font-family:SourceHanSansCN-Normal;color:rgba(77,77,77,1);
+            line-height:0.5rem;" @click="moreAnswerTwo(item)" >更多</p>
+            </div>
+          </div>
+
+          <div class="content" style="width:100%" v-show="flagTxt" ref="itemTwo">
+            <img src="../common/image/robot_profile.png" class="contentImg" style="margin-left:0;">
+            <div class="contentBox" style="width:70%;margin-right:0.9rem;">
+              <div v-for="(item,key) in answer">
+                <p align="left">{{item}}
+                </p>
+                <hr style="width:93%;border:0.5px solid rgba(131,205,93,1);margin:0 auto;">
+              </div>
+              <img src="../common/image/service_ears.png" alt="" style="float:left;margin-top:0.05rem;" @click="serviceDetail"><span
+              style="font-size:0.1rem;font-family:SourceHanSansCN-Normal;color:rgba(77,77,77,1);
+            float:left;line-height:1.10rem" @click="serviceDetail">转人工客服</span>
+              <p style="float:right;font-size:0.1rem;font-family:SourceHanSansCN-Normal;color:rgba(77,77,77,1);
+            line-height:0.5rem;" @click="addText">更多</p>
             </div>
           </div>
         </div>
       </div>
+
+
 
       <div class="bottom">
         <div class="bottomLeft"><p>{{text}}</p></div>
@@ -91,12 +167,16 @@ export default {
     for (var i = 0; i < 5; i++) {
       this.answer.push(this.answerArr[i])
     }
+      this.beforeflag = true
+      this.beforeQuest = true
   },
   data () {
     return {
       text: '检索',
       heart: require('../common/image/heart_before.png'),
+      heartBeat: require('../common/image/heart_before.png'),
       flag: true,
+      flagBeat:true,
       answerArr: ['1.猪肉营养价值在哪里?', '2.猪肉怎么吃最健康', '3.猪肉对我们有什么害处',
         '4.猪肉营养价值在哪里?', '5.猪肉怎么吃最健康', '6.猪肉营养价值在哪里?', '7.猪肉怎么吃最健康',
         '8.猪肉营养价值在哪里?', '9.猪肉怎么吃最健康', '10.猪肉怎么吃最健康', '11.猪肉怎么吃最健康',
@@ -116,20 +196,32 @@ export default {
       answerTxt:false,
       service:false,
       firstQuest:false,
-      Time:1
+      Time:1,
+      beforeTxt:['猪肉什么功效'],
+      beforeflag:false,
+      beforeQuest:false
     }
   },
   methods: {
     toIndex(){
       this.$router.go(-1)
     },
-    changeStyle(){
+    changeStyle(a){
       if (this.flag) {
-        this.heart = require('../common/image/heart_after.png')
+        this.$refs.changeSty[a].src = require('../common/image/heart_after.png')
         this.flag = false
       } else {
-        this.heart = require('../common/image/heart_before.png')
+        this.$refs.changeSty[a].src = require('../common/image/heart_before.png')
         this.flag = true
+      }
+    },
+    changeBeat(a){
+      if (this.flagBeat) {
+        this.$refs.changeBeat[a].src = require('../common/image/heart_after.png')
+        this.flagBeat = false
+      } else {
+        this.$refs.changeBeat[a].src = require('../common/image/heart_before.png')
+        this.flagBeat = true
       }
     },
     addText(){
@@ -180,17 +272,22 @@ export default {
           }
       }
     },
-    moreAnswer(){
-      this.flagTxt = true
+    moreAnswer(a){
+      console.log(this.$refs.item[a])
+      this.$refs.item[a].style.display = 'block'
+    },
+    moreAnswerTwo(a){
+      console.log(this.$refs.item[a])
+      this.$refs.itemTwo[a].style.display = 'block'
     },
     setQuest(){
-      if(this.Time == 1){
+      if(this.service == false){
         this.firstQuestTxt.push(this.questDetail)
         this.firstQuest = true
         this.answerTxt = true
-        this.Time =2
-      }else{
-        this.questTxt = []
+      }
+      this.Time ++
+      if(this.service == true){
         this.questTxt.push(this.questDetail)
         this.quest = true
         this.answerTxt = true
