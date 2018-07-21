625bdc0ee85a7bbadfddd76e5195ecff5cc55ccc
weipengmao <412948637@qq.com>
2018-07-21 10:02:09 +0800

    openid

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 //生产环境下参数配置
 module.exports = {
   NODE_ENV: '"production"',
-  API_HOST:'"http://www.health-vi.com/"'
+  API_HOST:'"http://robot.health-vi.com/"'
 }
--- src/components/index.vue
+++ src/components/index.vue
@@ -52,6 +52,7 @@ export default {
     // const appid = 'wx7e88240ec21db9f6'
     // const code = CustomerHttp.getUrlParam('code')
     // const local = window.location.href
+    // location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx7e88240ec21db9f6&redirect_uri=http://robot.health-vi.com/customer/auth.html?redirect_uri=http://robot.health-vi.com/customer&response_type=code&scope=snsapi_base&state=47c7be842c4e790f&component_appid=wxb98cc920f74c1f5f#wechat_redirect"
     CustomerHttp.httpPost(`${root}qx`,{"usr":"13600000001","pwd":"cfcd208495d565ef66e7dff9f98764da","cmd":"sys.login","ver":1})
     // if(code == null || code == ''){
     //     location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&
